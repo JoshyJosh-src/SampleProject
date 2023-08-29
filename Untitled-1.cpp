692cf1a4b1afa53513084d22809506ce35162e96
JoshyJosh-src <82190253+JoshyJosh-src@users.noreply.github.com>
2023-08-29 04:53:31 +0000

    tgdfgdfgdfgdg

--- Untitled-1.cpp
+++ Untitled-1.cpp
@@ -1,15 +1,7 @@
 using namespace std;
 
 int main(){
-<<<<<<< HEAD
 
     cout<< "Hellow World";
     cout << "sdasdasdads";
-}
-=======
->>>>>>> f3a3ab4 (222)
-
-    cout<< "Hellow World";
-
-    
 }
